2daea076840fa1e40afac298f8e6d492636af8e9
Code-EdIS <edoardo_ivan@outlook.it>
2025-12-18 22:31:21 +0100

    form di contatto

---
+++ email.js
@@ -0,0 +1,30 @@
+const form = document.querySelector(".Contattami");
+const pulsante = document.querySelector(".datiContatto");
+
+form.addEventListener("submit", (e) => {
+          e.preventDefault();
+          
+          pulsante.textContent="invio in corso...";
+          pulsante.disabled = true;
+          
+          const data = new FormData(form);
+          
+          fetch("https://formspree.io/f/xpwynpga", {
+            method: "POST",
+            body: data,
+            headers: {
+              "Accept": "application/json"
+            }
+          }).then(res => {
+            if(res.ok){
+              pulsante.textContent="ok";
+              form.reset();
+              }else{pulsante.textContent="errore di invio! riprovare"}
+            }
+          ).catch(() => {
+            pulsante.textContent="errore di connessione";
+          }).finally(() => {
+            pulsante.disabled = false;
+          }
+          )
+});
