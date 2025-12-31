b4d5ad18fbdb8d3fca10776e4627727895794c74
Code-EdIS <edoardo_ivan@outlook.it>
2025-12-31 08:31:14 +0100

    aggiunta indicazione dropdown

--- Animazioni.js
+++ Animazioni.js
@@ -1,15 +1,5 @@
-const formazioneDropdownCs = document.querySelector(".cardFormazioneCs");
-const formazioneDropDownFrontEnd = document.querySelector(".cardFormazioneFrontEnd");
-const formazioneDropDownIa = document.querySelector(".cardFormazioneIa");
-
-formazioneDropdownCs.addEventListener('click', () => {
-  formazioneDropdownCs.classList.toggle("altezzaCardCs");
-});
-
-formazioneDropDownFrontEnd.addEventListener('click', () => {
-  formazioneDropDownFrontEnd.classList.toggle("altezzaCardFrontEnd");
-});
-
-formazioneDropDownIa.addEventListener('click', () => {
-  formazioneDropDownIa.classList.toggle("altezzaCardIa");
+document.querySelectorAll(".cardFormazione").forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle("altezzaCard")
+  });
 });
