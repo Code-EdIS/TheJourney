c52bda12de79540471574ff617a55b3323cffc05
Code-EdIS <edoardo_ivan@outlook.it>
2026-01-01 19:07:13 +0100

    aggiunta base progetti poi un po' di estetica

--- Animazioni.js
+++ Animazioni.js
@@ -1,5 +1,13 @@
 document.querySelectorAll(".cardFormazione").forEach(card => {
   card.addEventListener('click', () => {
     card.classList.toggle("altezzaCard")
+    card.querySelector(".ditoPuntato").classList.toggle("giraDito")
+  });
+});
+
+document.querySelectorAll(".cardProgetto").forEach(card => {
+  card.addEventListener('click', () => {
+    card.classList.toggle("altezzaCard")
+    card.querySelector(".ditoPuntato").classList.toggle("giraDito")
   });
 });
