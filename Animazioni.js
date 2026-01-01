d5efc346e0175cd346b446fdc5dd394444283184
Code-EdIS <edoardo_ivan@outlook.it>
2026-01-01 19:18:01 +0100

    animazione dito

--- Animazioni.js
+++ Animazioni.js
@@ -1,13 +1,13 @@
 document.querySelectorAll(".cardFormazione").forEach(card => {
   card.addEventListener('click', () => {
-    card.classList.toggle("altezzaCard")
     card.querySelector(".ditoPuntato").classList.toggle("giraDito")
+    card.classList.toggle("altezzaCard")
   });
 });
 
 document.querySelectorAll(".cardProgetto").forEach(card => {
   card.addEventListener('click', () => {
-    card.classList.toggle("altezzaCard")
     card.querySelector(".ditoPuntato").classList.toggle("giraDito")
+    card.classList.toggle("altezzaCard")
   });
 });
